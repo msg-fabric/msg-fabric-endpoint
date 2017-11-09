3d7b0dbc44f8762f35d5641f85e21f5881ebb9cd
Shane Holloway <shane.holloway@ieee.org>
2017-11-08 17:30:48 -0700

    Starting point

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import rpi_babel from 'rollup-plugin-babel'
+
+const sourcemap = 'inline'
+
+const external = []
+
+const plugins = [jsy_plugin()]
+
+export default [
+	{ input: 'code/index.jsy',
+		output: [
+      { file: `dist/index.js`, format: 'cjs' },
+      { file: `dist/index.mjs`, format: 'es' },
+    ],
+    sourcemap, external, plugins },
+]
+
+
+
+
+function jsy_plugin() {
+  const jsy_preset = [ 'jsy/lean', { no_stage_3: true, modules: false } ]
+  return rpi_babel({
+    exclude: 'node_modules/**',
+    presets: [ jsy_preset ],
+    plugins: [],
+    babelrc: false }) }
