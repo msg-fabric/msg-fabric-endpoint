6a51fa9563f7a42e673c4a4083bc0313442f775b
Shane Holloway <shane.holloway@ieee.org>
2018-03-01 12:33:07 -0700

    Added ep_kinds extension mechanism. Added api_invoke extension

--- test/unit/ep_kinds_api.js
+++ test/unit/ep_kinds_api.js
@@ -21,10 +21,40 @@ describe @ 'ep_kinds.api', @=> ::
         return 'ans rpc_three'
 
 
-  describe @ 'api', @=> ::
+  describe @ 'api basics', @=> ::
     it @ 'basic create', @=>> ::
       const tgt = hub.endpoint.api @:
         /* ... api ... */
+
+    it @ 'basic create with normal init function', @=>> ::
+      const tgt = hub.endpoint.api @ () => ::
+        log @ 'init'
+        return @{} /* ... api ... */
+
+      expect(log.calls).to.deep.equal @#
+        'init'
+
+      await tgt
+
+      expect(log.calls).to.deep.equal @#
+        'init'
+
+
+    it @ 'basic create with async init function', @=>> ::
+      const tgt = hub.endpoint.api @ async () => ::
+        log @ 'init 0'
+        await 0
+        log @ 'init 1'
+        return @{} /* ... api ... */
+
+      expect(log.calls).to.deep.equal @#
+        'init 0'
+
+      await tgt
+
+      expect(log.calls).to.deep.equal @#
+        'init 0'
+        'init 1'
 
     it @ 'call one', @=>> ::
       const tgt = hub.endpoint.api @ test_api()
@@ -57,6 +87,91 @@ describe @ 'ep_kinds.api', @=> ::
         @{} fn: 'rpc_one', kw: {sz: 'a', list: [1,2,3], obj: {ans: 1942}}, ctx: '$ctx$'
 
 
+    it @ 'call one with normal init function', @=>> ::
+      const tgt = hub.endpoint.api @=> ::
+        log @ 'init'
+        return test_api()
+
+      expect(log.calls).to.deep.equal @#
+        'init'
+
+      await tgt.ready
+
+      const c = hub.endpoint.clientOf @ tgt, '$ctx$', async invoke => ::
+        log @ 'top of client'
+
+        let a1 = invoke @ 'rpc_one', @{} sz:'a', list: [1,2,3], obj: {ans: 1942}
+        log @ 'after a1 invoke', 'function' === typeof a1.then
+
+        a1 = await a1
+        log @ 'after await a1', a1
+
+        log @ 'end of client'
+
+
+      expect(log.calls).to.deep.equal @#
+        'init'
+      expect(api_log.calls).to.deep.equal @#
+
+      await c
+
+      expect(log.calls).to.deep.equal @#
+        'init'
+        'top of client'
+        @[] 'after a1 invoke', true
+        @[] 'after await a1', @{} answer: 'ans rpc_one'
+        'end of client'
+
+      expect(api_log.calls).to.deep.equal @#
+        @{} fn: 'rpc_one', kw: {sz: 'a', list: [1,2,3], obj: {ans: 1942}}, ctx: '$ctx$'
+
+
+
+    it @ 'call one with async init function', @=>> ::
+      const tgt = hub.endpoint.api @=>> ::
+        log @ 'init 0'
+        await 0
+        log @ 'init 1'
+        return test_api()
+
+      expect(log.calls).to.deep.equal @#
+        'init 0'
+
+      await tgt.ready
+
+      const c = hub.endpoint.clientOf @ tgt, '$ctx$', async invoke => ::
+        log @ 'top of client'
+
+        let a1 = invoke @ 'rpc_one', @{} sz:'a', list: [1,2,3], obj: {ans: 1942}
+        log @ 'after a1 invoke', 'function' === typeof a1.then
+
+        a1 = await a1
+        log @ 'after await a1', a1
+
+        log @ 'end of client'
+
+
+      expect(log.calls).to.deep.equal @#
+        'init 0'
+        'init 1'
+      expect(api_log.calls).to.deep.equal @#
+
+      await c
+
+      expect(log.calls).to.deep.equal @#
+        'init 0'
+        'init 1'
+        'top of client'
+        @[] 'after a1 invoke', true
+        @[] 'after await a1', @{} answer: 'ans rpc_one'
+        'end of client'
+
+      expect(api_log.calls).to.deep.equal @#
+        @{} fn: 'rpc_one', kw: {sz: 'a', list: [1,2,3], obj: {ans: 1942}}, ctx: '$ctx$'
+
+
+
+  describe @ 'error messages', @=> ::
 
     it @ 'call method without prefix', @=>> ::
       const tgt = hub.endpoint.api @ test_api()
@@ -66,6 +181,32 @@ describe @ 'ep_kinds.api', @=> ::
 
       const c = await hub.endpoint.clientOf @ tgt, '$ctx$', async invoke => ::
         const a1 = await invoke @ 'one'
+        log @ 'error', a1.op, a1.error
+
+        expect @ a1.err_from
+        .to.be.a @ 'function'
+        .to.have.a.property @ sym_sampi
+
+        expect @ a1.err_from[sym_sampi]
+        .to.be.a @ 'string'
+        .to.equal @ tgt[sym_sampi]
+
+
+      expect(api_log.calls).to.deep.equal @#
+
+      expect(log.calls).to.deep.equal @#
+        @[] 'error', 'one', @{} code: 404, message: 'Unknown operation'
+
+
+
+    it @ 'call method using proxy without prefix', @=>> ::
+      const tgt = hub.endpoint.api @ test_api()
+      expect(tgt).to.have.a.property @ sym_sampi
+
+      await tgt.ready
+
+      const c = await hub.endpoint.clientOf @ tgt, '$ctx$', async invoke => ::
+        const a1 = await invoke.one()
         log @ 'error', a1.op, a1.error
 
         expect @ a1.err_from
@@ -110,87 +251,91 @@ describe @ 'ep_kinds.api', @=> ::
 
 
 
-  describe @ 'parallel', @=> ::
-    it @ 'basic create', @=>> ::
-      const tgt = hub.endpoint.api_parallel @:
-        /* ... api ... */
+  api_parallel_variation('api')
+  api_parallel_variation('api_parallel')
+
+  function api_parallel_variation(variation, api_key=variation) ::
+    return describe @ `invoke with ${variation}`, @=> ::
+      it @ 'basic create', @=>> ::
+        const tgt = hub.endpoint[api_key] @:
+          /* ... api ... */
 
 
-    it @ 'call parallel one two three', @=>> ::
-      const tgt = hub.endpoint.api_parallel @ test_api()
-      await tgt.ready
+      it @ 'call one two three', @=>> ::
+        const tgt = hub.endpoint[api_key] @ test_api()
+        await tgt.ready
 
-      const c = await hub.endpoint.clientOf @ tgt, '$ctx$', async invoke => ::
-        api_log @ 'top'
-        const p_one = invoke @ 'rpc_one', @{} order: 1
-        api_log @ 'invoked one'
-        const p_two = invoke @ 'rpc_two', @{} order: 2
-        api_log @ 'invoked two'
-        const p_three = invoke @ 'rpc_three', @{} order: 3
-        api_log @ 'invoked three'
+        const c = await hub.endpoint.clientOf @ tgt, '$ctx$', async invoke => ::
+          api_log @ 'top'
+          const p_one = invoke @ 'rpc_one', @{} order: 1
+          api_log @ 'invoked one'
+          const p_two = invoke @ 'rpc_two', @{} order: 2
+          api_log @ 'invoked two'
+          const p_three = invoke @ 'rpc_three', @{} order: 3
+          api_log @ 'invoked three'
 
-        api_log @ 'awaited one', await p_one
-        api_log @ 'awaited two', await p_two
-        api_log @ 'awaited three', await p_three
-
-
-      expect(api_log.calls).to.deep.equal @#
-        'top'
-        'invoked one'
-        'invoked two'
-        'invoked three'
-        @{} fn: 'rpc_one', kw: { order: 1 }, ctx: '$ctx$'
-        @{} fn: 'rpc_two', kw: { order: 2 }, ctx: '$ctx$'
-        @{} fn: 'rpc_three', kw: {order: 3}, ctx: '$ctx$'
-        'two await 0'
-        @[] 'awaited one', @{} answer: 'ans rpc_one'
-        @[] 'awaited two', @{} answer: 'ans rpc_two'
-        @[] 'awaited three', @{} answer: 'ans rpc_three'
+          api_log @ 'awaited one', await p_one
+          api_log @ 'awaited two', await p_two
+          api_log @ 'awaited three', await p_three
 
 
-    it @ 'call serial one two three', @=>> ::
-      const tgt = hub.endpoint.api_parallel @ test_api()
-      await tgt.ready
-
-      const c = await hub.endpoint.clientOf @ tgt, '$ctx$', async invoke => ::
-        api_log @ 'top'
-        const p_one = invoke @ 'rpc_one', @{} order: 1
-        api_log @ 'invoked one'
-        api_log @ 'awaited one', await p_one
-
-        const p_two = invoke @ 'rpc_two', @{} order: 2
-        api_log @ 'invoked two'
-        api_log @ 'awaited two', await p_two
-
-        const p_three = invoke @ 'rpc_three', @{} order: 3
-        api_log @ 'invoked three'
-        api_log @ 'awaited three', await p_three
+        expect(api_log.calls).to.deep.equal @#
+          'top'
+          'invoked one'
+          'invoked two'
+          'invoked three'
+          @{} fn: 'rpc_one', kw: { order: 1 }, ctx: '$ctx$'
+          @{} fn: 'rpc_two', kw: { order: 2 }, ctx: '$ctx$'
+          @{} fn: 'rpc_three', kw: {order: 3}, ctx: '$ctx$'
+          'two await 0'
+          @[] 'awaited one', @{} answer: 'ans rpc_one'
+          @[] 'awaited two', @{} answer: 'ans rpc_two'
+          @[] 'awaited three', @{} answer: 'ans rpc_three'
 
 
-      expect(api_log.calls).to.deep.equal @#
-        'top'
-        'invoked one'
-        @{} fn: 'rpc_one', kw: { order: 1 }, ctx: '$ctx$'
-        @[] 'awaited one', @{} answer: 'ans rpc_one'
+      it @ 'call serial one two three', @=>> ::
+        const tgt = hub.endpoint[api_key] @ test_api()
+        await tgt.ready
 
-        'invoked two'
-        @{} fn: 'rpc_two', kw: { order: 2 }, ctx: '$ctx$'
-        'two await 0'
-        @[] 'awaited two', @{} answer: 'ans rpc_two'
+        const c = await hub.endpoint.clientOf @ tgt, '$ctx$', async invoke => ::
+          api_log @ 'top'
+          const p_one = invoke @ 'rpc_one', @{} order: 1
+          api_log @ 'invoked one'
+          api_log @ 'awaited one', await p_one
 
-        'invoked three'
-        @{} fn: 'rpc_three', kw: {order: 3}, ctx: '$ctx$'
-        @[] 'awaited three', @{} answer: 'ans rpc_three'
+          const p_two = invoke @ 'rpc_two', @{} order: 2
+          api_log @ 'invoked two'
+          api_log @ 'awaited two', await p_two
+
+          const p_three = invoke @ 'rpc_three', @{} order: 3
+          api_log @ 'invoked three'
+          api_log @ 'awaited three', await p_three
+
+
+        expect(api_log.calls).to.deep.equal @#
+          'top'
+          'invoked one'
+          @{} fn: 'rpc_one', kw: { order: 1 }, ctx: '$ctx$'
+          @[] 'awaited one', @{} answer: 'ans rpc_one'
+
+          'invoked two'
+          @{} fn: 'rpc_two', kw: { order: 2 }, ctx: '$ctx$'
+          'two await 0'
+          @[] 'awaited two', @{} answer: 'ans rpc_two'
+
+          'invoked three'
+          @{} fn: 'rpc_three', kw: {order: 3}, ctx: '$ctx$'
+          @[] 'awaited three', @{} answer: 'ans rpc_three'
 
 
 
-  describe @ 'inorder', @=> ::
+  describe @ 'invoke with api_inorder', @=> ::
     it @ 'basic create', @=>> ::
       const tgt = hub.endpoint.api_inorder @:
         /* ... api ... */
 
 
-    it @ 'call parallel one two three', @=>> ::
+    it @ 'call one two three', @=>> ::
       const tgt = hub.endpoint.api_inorder @ test_api()
       await tgt.ready
 
@@ -258,3 +403,74 @@ describe @ 'ep_kinds.api', @=> ::
         'invoked three'
         @{} fn: 'rpc_three', kw: {order: 3}, ctx: '$ctx$'
         @[] 'awaited three', @{} answer: 'ans rpc_three'
+
+
+  describe @ 'invoke proxy api', @=> ::
+    it @ 'call one two three', @=>> ::
+      const tgt = hub.endpoint.api @ test_api()
+      await tgt.ready
+
+      const c = await hub.endpoint.clientOf @ tgt, '$ctx$', async invoke => ::
+        api_log @ 'top'
+        const p_one = invoke.rpc_one @: order: 1
+        api_log @ 'invoked one'
+        const p_two = invoke.rpc_two @: order: 2
+        api_log @ 'invoked two'
+        const p_three = invoke.rpc_three @: order: 3
+        api_log @ 'invoked three'
+
+        api_log @ 'awaited one', await p_one
+        api_log @ 'awaited two', await p_two
+        api_log @ 'awaited three', await p_three
+
+
+      expect(api_log.calls).to.deep.equal @#
+        'top'
+        'invoked one'
+        'invoked two'
+        'invoked three'
+        @{} fn: 'rpc_one', kw: { order: 1 }, ctx: '$ctx$'
+        @{} fn: 'rpc_two', kw: { order: 2 }, ctx: '$ctx$'
+        @{} fn: 'rpc_three', kw: {order: 3}, ctx: '$ctx$'
+        'two await 0'
+        @[] 'awaited one', @{} answer: 'ans rpc_one'
+        @[] 'awaited two', @{} answer: 'ans rpc_two'
+        @[] 'awaited three', @{} answer: 'ans rpc_three'
+
+
+    it @ 'call serial one two three', @=>> ::
+      const tgt = hub.endpoint.api @ test_api()
+      await tgt.ready
+
+      const c = await hub.endpoint.clientOf @ tgt, '$ctx$', async invoke => ::
+        api_log @ 'top'
+        const p_one = invoke.rpc_one @: order: 1
+        api_log @ 'invoked one'
+        api_log @ 'awaited one', await p_one
+
+        const p_two = invoke.rpc_two @: order: 2
+        api_log @ 'invoked two'
+        api_log @ 'awaited two', await p_two
+
+        const p_three = invoke.rpc_three @: order: 3
+        api_log @ 'invoked three'
+        api_log @ 'awaited three', await p_three
+
+
+      expect(api_log.calls).to.deep.equal @#
+        'top'
+        'invoked one'
+        @{} fn: 'rpc_one', kw: { order: 1 }, ctx: '$ctx$'
+        @[] 'awaited one', @{} answer: 'ans rpc_one'
+
+        'invoked two'
+        @{} fn: 'rpc_two', kw: { order: 2 }, ctx: '$ctx$'
+        'two await 0'
+        @[] 'awaited two', @{} answer: 'ans rpc_two'
+
+        'invoked three'
+        @{} fn: 'rpc_three', kw: {order: 3}, ctx: '$ctx$'
+        @[] 'awaited three', @{} answer: 'ans rpc_three'
+
+
+
